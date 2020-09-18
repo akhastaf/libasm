0604811dbdc9c2ae489405764a3c7b3701d0dffb
Abderrazzaq khastaf <abderrazzaqkhastaf@gmail.com>
2020-09-18 11:06:00 -0400

    ft_strdup.s

--- ft_strdup.s
+++ ft_strdup.s
@@ -3,16 +3,19 @@ global  ft_strdup
 extern malloc
 ; rdi src
 ft_strdup:
+            mov r8, 0
             cmp rdi, 0
             je error
 len:
             mov rcx, 0
             jmp len_compare
-len_increment:
+increment:
             inc rcx
+            cmp r8, 1
+            je copy_compare
 len_compare:
             cmp BYTE[rdi + rcx], 0
-            jne len_increment
+            jne increment
 alloc:
             push rdi
             inc rcx
@@ -23,14 +26,14 @@ alloc:
             pop rdi
 copy:
             mov rcx, 0
+            mov r8, 1
             jmp copy_compare
-copy_increment:
-                inc rcx
+
 copy_compare:
             mov dl, BYTE[rdi + rcx]
             mov BYTE[rax + rcx], dl
             cmp BYTE[rdi + rcx], 0
-            jne copy_increment
+            jne increment
             jmp return
 return:
         ret
